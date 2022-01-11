74d1e6bac8ffc3c0dc0f3ac77854a2dc12a80c84
Clifford18 <cliffordjohn.cj@gmail.com>
2022-01-11 14:30:58 +0300

    update app.js for GCD

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const {sum_to_n_iterative} =require("./sum_to_n_iterative")
 const {sum_to_n} = require("./sum_to_n.js")
+const {gcd} = require("./gcd.js")
 console.log("test")
 
 sum_to_n_iterative(5);
 
-console.log("sum recursively :" + sum_to_n(-1))
+console.log("sum recursively :" + sum_to_n(4))
+
+
+console.log("GCD of 12 and 24 is: " + gcd(12,24));
+console.log("GCD of 12 and 27 is:" + gcd(12, 27));
