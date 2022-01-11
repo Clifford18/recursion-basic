661617263cfcfd11b2d40348b573cf9ea2df55fc
Clifford18 <cliffordjohn.cj@gmail.com>
2022-01-11 13:49:23 +0300

    update app.js file on sum_to_n file

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const {sum_to_n_iterative} =require("./sum_to_n_iterative")
+const {sum_to_n} = require("./sum_to_n.js")
 console.log("test")
 
 sum_to_n_iterative(5);
+
+console.log("sum recursively :" + sum_to_n(-1))
