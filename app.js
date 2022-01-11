b9faf4638aef5f001757bfc783d4de9fcb693b8d
Clifford18 <cliffordjohn.cj@gmail.com>
2022-01-11 15:05:44 +0300

    update app.js file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const {sum_to_n_iterative} =require("./sum_to_n_iterative")
-const {sum_to_n} = require("./sum_to_n.js")
-const {gcd} = require("./gcd.js")
-console.log("test")
+const {sum_to_n_iterative} = require("./sum_to_n_iterative");
+const {sum_to_n} = require("./sum_to_n.js");
+const {gcd} = require("./gcd.js");
+const {print_backwards} =require("./print_backwards.JS")
+console.log("test");
 
 sum_to_n_iterative(5);
 
-console.log("sum recursively :" + sum_to_n(4))
+console.log("sum recursively :" + sum_to_n(4));
 
 
-console.log("GCD of 12 and 24 is: " + gcd(12,24));
+console.log("GCD of 12 and 24 is: " + gcd(12, 24));
 console.log("GCD of 12 and 27 is:" + gcd(12, 27));
+
+console.log("Printing backward th word HELLO  : " + print_backwards("HELLO"));
